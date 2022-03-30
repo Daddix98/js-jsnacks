a2db698bfdf0a72e50ebd351aa5f009252bfa6a9
Davide Scriboni <davidescriboni94@gmail.com>
2022-03-30 11:10:48 +0200

    aggiornamento

---
+++ js-jsnacks-7/script.js
@@ -0,0 +1,29 @@
+   
+   
+    function cubo(n) {
+        let array = new Array();
+    
+        for (let i = 0; i < n; i++) {
+            array.push(iii);
+        }
+        console.log(array);
+        return array;
+    }
+    
+    let input = prompt("Inserisci un numero: ");
+    
+    alert(cubo + input)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+  
