5c4c8ca0b02209316c8511cba04e8765edaaf42e
Davide Scriboni <davidescriboni94@gmail.com>
2022-03-30 21:05:43 +0200

    fine-esercizio-7

--- js-jsnacks-7/script.js
+++ js-jsnacks-7/script.js
@@ -1,18 +1,27 @@
-   
-   
-    function cubo(n) {
-        let array = new Array();
-    
-        for (let i = 0; i < n; i++) {
-            array.push(iii);
-        }
-        console.log(array);
-        return array;
+//creo un prompt da dove l'utente inserirà il numero che successivamente calcoleremo al cubo.
+
+let nUtente = prompt("inserisci un numero"); 
+
+//creo una variabile che richiama la funzione "CUBO" da cui ricavare il risultato del calcolo. 
+
+let risultato = (cubo(nUtente));
+
+//creo la funzione che calcola il numero (n) elevato al cubo dove (n) corrisponde al valore che l'utente ha inserito.
+
+function cubo(){
+  
+    let calcoloAlCubo = nUtente * nUtente * nUtente;
+    if(nUtente = calcoloAlCubo){
+    alert("il risultato è " + (calcoloAlCubo));
     }
+     
     
-    let input = prompt("Inserisci un numero: ");
+
     
-    alert(cubo + input)
+}
+ 
+
+
     
 
 
